5eec624767aa0e1a32a5c63272580cb20346b4f6
Rahul Bansal <rahul.bansal@ofbusiness.in>
2018-05-12 18:43:01 +0530

    change to request

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-var https = require('https');
+var request = require('request');
 var express = require('express');
 var cors = require('cors');
 var app = express();
@@ -17,19 +17,14 @@ app.get('/grab/:videoId', function (req, res) {
     // youtube2mp3api.com/@grab?vidID=kw4tT7SCmaY&format=mp3&streams=mp3&api=button
     var videoId = req.params.videoId;
     var options = {
-        host: 'youtube2mp3api.com',
-        port: 443,
-        path: '/@grab?vidID=' + videoId + '&format=mp3&streams=mp3&api=button',
+        method: 'GET',
+        url: 'https://youtube2mp3api.com/@grab?vidID=' + videoId + '&format=mp3&streams=mp3&api=button',
         headers: {
             referer: 'https://youtube2mp3api.com/@api/button/mp3/' + videoId,
         }
     };
-    https.get(options, (_res) => {
-        _res.on('data', function (chunk) {
-            res.write(chunk)
-        });
-    }).on('error', (e) => {
-        console.error(e);
+    request(options, function (error, response, body) {
+        res.send(body)
     });
 })
 
